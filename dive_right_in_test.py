62e625156ba077c5418ced919e3c091db76621dc
ssarber <ssarber@gmail.com>
2020-12-31 13:59:51 -0800

    Add EvilCorp test

--- dive_right_in_test.py
+++ dive_right_in_test.py
@@ -1,39 +1,63 @@
+#!/usr/bin/env python
+
+import argparse
+import sys
 import time
 import unittest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
 
 
-class GoogleSearchMacTest(unittest.TestCase):
+class EvilCorpSiteNavigationTest(unittest.TestCase):
 
     def setUp(self):
-        self.driver = webdriver.Safari()
+        if run_on_ci:
+            chrome_options = Options()
+            chrome_options.add_argument("--no-sandbox")
+            chrome_options.add_argument("--disable-dev-shm-usage")
+            chrome_options.add_argument("--headless")
+            self.driver = webdriver.Chrome(options=chrome_options)
 
-        # If you are on Windows, delete the line above and uncomment the one below :)
-        # self.driver = webdriver.Chrome("C:\chromedriver.exe")
+        if platform == "Mac" and not run_on_ci:
+            self.driver = webdriver.Safari()
+        elif platform == "Windows":
+            self.driver = webdriver.Chrome("C:\chromedriver.exe")
+
+    def tearDown(self):
+        self.driver.close()
         
-    def test_search_returns_expected_result(self):
+    def test_evil_corp_site_navigation(self):
         driver = self.driver
-        driver.get("http://www.google.com")
+        driver.get("https://evil-corp.carrd.co")
+        self.wait_for(seconds=2)
+        # driver.find_element_by_id('text04').click()
 
-        search_text_field = driver.find_element_by_name("q")
-        search_text_field.send_keys("Let me google that for you")
-
+        driver.find_element_by_css_selector("#buttons02 .n01").click()
         self.wait_for(seconds=3)
-
-        search_text_field.send_keys(Keys.RETURN)
-
+        driver.find_element_by_css_selector("#buttons03 .button").click()
         self.wait_for(seconds=3)
-
-        expected_result = "LMGTFY - Search Made Easy"
-
-        assert expected_result in driver.page_source
+        driver.find_element_by_css_selector("li:nth-child(2) > .button").click()
+        self.wait_for(seconds=3)
+        driver.find_element_by_css_selector("#buttons05 .label").click()
+        self.wait_for(seconds=3)
+        driver.find_element_by_css_selector("li:nth-child(3) > .button").click()
+        self.wait_for(seconds=3)
+        driver.find_element_by_css_selector("#buttons01 svg").click()
+        self.wait_for(seconds=3)
 
     def wait_for(self, seconds=0):
         time.sleep(seconds)
 
-    def tearDown(self):
-        self.driver.close()
-
 if __name__ == "__main__":
-    unittest.main()
+    global run_on_ci
+    global platform
+
+    parser = argparse.ArgumentParser(add_help=False)
+    parser.add_argument('--run-on-ci', action='store_true')
+    parser.add_argument('--platform', choices=['Windows', 'Mac'], default='Mac')
+    options, args = parser.parse_known_args()
+
+    run_on_ci = options.run_on_ci
+    platform = options.platform
+    unittest.main(argv=sys.argv[:1] + args)
